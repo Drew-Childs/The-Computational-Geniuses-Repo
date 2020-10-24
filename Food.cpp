f749772dc5992c92eeccd90ca09e74c7e1c2823e
jhtwhb <jhtwhb@mail.umkc.edu>
2020-10-24 00:13:54 -0500

    Removed a variable

--- Food.cpp
+++ Food.cpp
@@ -4,12 +4,10 @@ Food::Food() {
 	food = "";
 	recipe.resize(0);
 	isFavorite = false;
-	history = 0;
 }
 
 Food::Food(string name, vector<ingred> recipe) {
 	this->food = name;
 	this->recipe = recipe;
 	isFavorite = false;
-	history = 0;
 }
